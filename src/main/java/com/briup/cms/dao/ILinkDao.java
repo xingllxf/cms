6164eed87ab8208c4daed1924f944f05aa227eeb
xingll <1244697297@qq.com>
2019-11-25 22:20:36 +0800

    初始化项目

---
+++ src/main/java/com/briup/cms/dao/ILinkDao.java
@@ -0,0 +1,10 @@
+package com.briup.cms.dao;
+
+
+
+import com.briup.cms.bean.LinK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILinkDao extends JpaRepository<LinK,Integer> {
+
+}
